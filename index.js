4df00d48c8d2ada876f6d7ed433d8ee1db4eee6f
Kevin Ayala <44719846+Kayala47@users.noreply.github.com>
2020-04-27 18:47:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//hey there! I've deployed this at: https://redirect.kayala.workers.dev
-
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -10,14 +8,14 @@ addEventListener('fetch', event => {
  */
 async function handleRequest(request) {
 
-  const init = {
-    headers: {
-      'content-type': 'text/html;charset=UTF-8'
-    }
-  }
-  
-  let url = "https://cfw-takehome.developers.workers.dev/api/variants"
+const cookie = getCookie(request, "url")
+var output
+var cookieNotice = ""
 
+if (!cookie){
+  //if we don't have a cookie with the name url, have to generate a new one
+    
+let url = "https://cfw-takehome.developers.workers.dev/api/variants"
 
 let resp = await fetch(url)
 .then((response) => response.text())
@@ -28,8 +26,15 @@ let resp = await fetch(url)
 var processed = resp.substring(resp.indexOf("[") + 1, resp.indexOf("]"))
 var choices = processed.split(",")
 var randomNum = Math.random() < 0.5 ? 0 : 1
-var output = choices[randomNum]
+output  = choices[randomNum]
 
+}else{
+
+  output = cookie
+  cookieNotice = `<p> Note: It looks like you've visitied this site before.
+   You'll be sent to the same site every time unless you delete your cookies :) </p> `
+  
+}
 
 
 html = `
@@ -37,19 +42,54 @@ html = `
   <body>
   <h1>Hey there! Thanks for considering my appliation! Here's your link:</h1>
   <a href=` + output + `> Follow Me! </a> <br> 
-  <p> Don't worry, you're being sent to ` + output + `</p> 
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/y6Sxv-sUYtM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+  <p> Don't worry, you're being sent to ` + output + `</p> ` + cookieNotice + `
+  <iframe width="560" height="315" 
+  src="https://www.youtube.com/embed/y6Sxv-sUYtM?start=13" 
+  frameborder="0" allow="accelerometer; 
+  autoplay; encrypted-media; gyroscope; 
+  picture-in-picture" allowfullscreen></iframe>
   </body>
 </html>
 `
 
 let respText = "Hiya! Here's your link: " + output
 
-// document.innerHTML = html
+const cookieText = "url=" + output
+
+const init = {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+      'Set-Cookie' : cookieText
+    }
+  }
 
 // return new Response(output, {status: 200})
 
-return new Response(html, init)
+
+return  new Response(html, init)
+
+
+}
+
+/**
+ * Credit to: https://developers.cloudflare.com/workers/templates/pages/cookie_extract/
+ * Grabs the cookie with name from the request headers
+ * @param {Request} request incoming Request
+ * @param {string} name of the cookie to grab
+ */
+function getCookie(request, name) {
+  let result = null
+  let cookieString = request.headers.get('Cookie')
+  if (cookieString) {
+    let cookies = cookieString.split(';')
+    cookies.forEach(cookie => {
+      let cookieName = cookie.split('=')[0].trim()
+      if (cookieName === name) {
+        let cookieVal = cookie.split('=')[1]
+        result = cookieVal
+      }
+    })
+  }
+  return result
 }
 
